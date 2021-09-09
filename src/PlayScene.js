b99f26a19f13a9a7459e5161eac12cb3872f2a82
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-09 14:01:10 +0200

    ✨ PlayScene: Randomize ball position and velocity

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -23,17 +23,32 @@ export default class PlayScene extends Phaser.Scene {
     // Ball
     let ball = this.add.rectangle(
       gameConfig.width / 2,
-      gameConfig.height / 2,
+      this.getStartingY(),
       20,
       20,
       0xffffff
     );
+
+    const ballVector = this.getStartingVector();
+
     this.physics.add
       .existing(ball)
-      .body.setVelocity(200, 100) // TODO: Randomize velocity
+      .body.setVelocity(ballVector.x, ballVector.y)
       .setBounce(1, 1)
       .setCollideWorldBounds(true);
   }
 
   update() {}
+
+  getStartingY() {
+    return Math.random() * gameConfig.height;
+  }
+
+  getStartingVector() {
+    let x = Math.random() * 200 + 200;
+    if (this.state.lastWinner === 2) x *= -1;
+    const y = Math.random() * 800 - 400;
+
+    return { x, y };
+  }
 }
